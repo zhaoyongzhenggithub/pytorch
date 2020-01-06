1f9436b436fc7317d4499e7363e80e25e2772c9e
zhaoyongzhenggithub <43742560+zhaoyongzhenggithub@users.noreply.github.com>
2020-01-06 17:39:49 +0800

    2020-01-06
    first change

--- mnist_train.py
+++ mnist_train.py
@@ -17,6 +17,15 @@ train_loader = torch.utils.data.DataLoader(
                                    torchvision.transforms.Normalize(
                                        (0.1307,), (0.3081,))
                                ])),
+    batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('mnist_data/', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
     batch_size=batch_size, shuffle=False)
 
 x, y = next(iter(train_loader))
@@ -53,3 +62,44 @@ train_loss = []
 for epoch in range(3):
 
     for batch_idx, (x, y) in enumerate(train_loader):
+
+        # x:[b, 1,28, 28], y: [512]
+        # [b, 1, 28, 28] => [b, 784]
+        x = x.view(x.size(0), 28 * 28)
+        # => [b, 10]
+        out = net(x)
+        #[b, 10]
+        y_onehot = one_hot(y)
+        # loss = mse(out, y_onehot)
+        loss = F.mse_loss(out, y_onehot)
+
+        optimizer.zero_grad()
+        loss.backward()
+        # w' = w - lr * grad
+        optimizer.step()
+
+        train_loss.append(loss.item())
+
+        if batch_idx % 10 == 0:
+            print(epoch, batch_idx, loss.item())
+
+plot_curve(train_loss)
+# we get optimal [w1, b1, w2, b2, w3, b3]
+
+total_correct = 0
+for x, y in test_loader:
+    x = x.view(x.size(0), 28 * 28)
+    out = net(x)
+    # out : [b, 10] => pred: [b]
+    pred = out.argmax(dim=1)
+    correct = pred.eq(y).sum().float().item()
+    total_correct += correct
+
+total_num = len(test_loader.dataset)
+acc = total_correct / total_num
+print('test acc:', acc)
+
+x, y = next(iter(test_loader))
+out = net(x.view(x.size(0), 28 * 28))
+pred = out.argmax(dim=1)
+plot_image(x, pred, 'test')
